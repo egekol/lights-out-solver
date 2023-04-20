e3148fc2a09c87c93ddcd251c6788d583e249f4d
egekol <kol18@itu.edu.tr>
2023-04-20 17:26:42 +0300

    [Added] LevelScore

---
+++ Assets/Scripts/UI/Puzzle/LevelScore.cs
@@ -0,0 +1,32 @@
+﻿// 20042023
+
+using System;
+using Managers;
+using TMPro;
+using UnityEngine;
+using Utilities;
+
+namespace UI.Puzzle
+{
+    public class LevelScore : MonoBehaviour
+    {
+        private TextMeshProUGUI textMeshProUGUI;
+        public TextMeshProUGUI LevelTMP => textMeshProUGUI ??= GetComponentInChildren<TextMeshProUGUI>(true);
+
+        private void OnEnable()
+        {
+            SetLevelText();
+            LevelManager.LevelComplete += SetLevelText;
+        }
+
+        private void OnDisable()
+        {
+            LevelManager.LevelComplete -= SetLevelText;
+        }
+
+        private void SetLevelText()
+        {
+            LevelTMP.text = $"LV{PlayerPrefKeys.CurrentPuzzleLevel.ToString()}";
+        }
+    }
+}
