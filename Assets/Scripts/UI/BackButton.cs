02a8a425b8512def84d599ad3416b1fa58e4050e
egekol <kol18@itu.edu.tr>
2023-04-24 02:29:40 +0300

    [Changed] Some Adjustments

--- Assets/Scripts/UI/BackButton.cs
+++ Assets/Scripts/UI/BackButton.cs
@@ -2,6 +2,7 @@
 
 using System;
 using Managers;
+using UI.CanvasGroups;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,10 +10,27 @@ namespace UI
 {
     public class BackButton : MonoBehaviour
     {
-
-
         private Button button;
         public Button Button => button ??= GetComponent<Button>();
 
+        private CanvasGroupSceneTransition canvasGroupSceneTransition;
+
+        public CanvasGroupSceneTransition CanvasGroupSceneTransition => canvasGroupSceneTransition ??=
+            GetComponentInParent<CanvasGroupSceneTransition>(true);
+
+        private void OnEnable()
+        {
+            Button.onClick.AddListener(GoToMenu);
+        }
+
+        private void OnDisable()
+        {
+            Button.onClick.RemoveListener(GoToMenu);
+        }
+
+        private void GoToMenu()
+        {
+            CanvasGroupSceneTransition.ChangeSceneToMenu();
+        }
     }
 }
